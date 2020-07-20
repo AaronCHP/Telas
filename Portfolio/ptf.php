8db52ff4d1c46bb9b64ed0ddcef5ee9639f81eb4
AaronCHP <aaronpark.park@gmail.com>
2020-07-20 12:04:48 -0300

    first commit

---
+++ Portfolio/ptf.php
@@ -0,0 +1,76 @@
+<!Doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">]
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/index.css">
+        <title>Teste</title>
+    </head>
+    <body>
+        <header>
+        </header>
+        <div class="menu">
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Quem sou eu</a></li>
+                    <li><a href="#">Habilidades</a></li>
+                    <li><a href="#">Destaques</a></li>
+                    <li><a href="#">Projetos</a></li>
+                    <li><a href="#">contatos</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="texto">
+            <section>
+                <article>
+                    <div class="apresentacao">
+                        <h2>Quem sou eu?</h2>
+                        <br/>
+                        <p>
+                            Sou um estudante do curso de Análise e Desenvolvimento de Sistemas da Faculdade
+                            de Tecnologia de São Paulo ( FATEC-SP ). Estou em busca por oportunidades de emprego
+                            e estágio, a fim de ganhar experiência e entrar no mercado de trabalho.
+                        </p>
+                        <br/>
+                        <h2>O que eu sei fazer?</h2>
+                        <br/>
+                        <p>
+                            Eu possuo conhecimento sobre:
+                        </p>
+                        <br/>
+                        <!-- pode mudar pra lista dentro de lista e dividir por níveis, linguagens, etc-->
+                        <ul>
+                            <li>Plataforma .Net Framework e Core– nível intermediário</li>
+                            <li>Banco de Dados SQL Server – nível intermediário</li>
+                            <li>Python, C#, HTML5 e CSS3 – nível intermediário</li>
+                            <li>Java, PHP e Javascript – nível básico</li>
+                            <li>Versionamento e gerenciamento de projetos via Git/Github</li>
+                            <li>Sistemas Operacionais Windows e Linux (nível básico)</li>
+                            <li>Arquiteturas de Software: MVC, Layered e Orientação a Objeto</li>
+                            <li>Lógica de Programação, Estrutura de Dados e Análise de Algoritmo</li>
+                            <li>Pacote Office</li>
+                        </ul>
+                        <br/>
+                        <h2>Idiomas</h2>
+                        <p>
+                            
+                        </p>
+                    </div>
+                </article>
+            </section>
+        </div>
+        <footer>
+            <div class="contato">
+                <ul>
+                    <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i>
+                    <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i>
+                    <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i>
+                    <li><a href="#"><i class="fa fa-github" aria-hidden="true"></i>
+                    <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i>
+                    </a></li>
+                </ul>
+            </div>
+        </footer>
+    </body>
+</html>
